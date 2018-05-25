7ade227eb933616f7968ca7434c511d9c2e91298
DComicsBP <dayonne.p@gmail.com>
2018-05-25 16:56:34 -0300

    organizando os grids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'app';
   src = '../assets/images/ifrs-google-maps.PNG';
   estilo = '';
 
-  onclickEvent() {
-    this.estilo = 'contrast';
+  onclickEvent(contrast) {
+    if (this.estilo === 'contrast') { this.estilo = ''; } else if (this.estilo === '') { this.estilo = contrast; }
   }
 
 }
