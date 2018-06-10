d7697b3696e4328a60591f1ebb6ac20c26c7727e
DComicsBP <dayonne.p@gmail.com>
2018-06-10 19:39:55 -0300

    deploy heroku

--- server.js
+++ server.js
@@ -1,54 +1,15 @@
-/*var express = require('express');
-var bodyPasser = require('body-parser');
-var path = require('path');
-var http = require('http');
-var app  = express();
+const express = require ('express');
+const path = require ('path');
 
-//var api = require('./server/api');
+const app = express ();
 
-app.use(bodyPasser.json());
-app.use(bodyPasser.urlencoded({extend: false}));
+// Serve apenas os arquivos estáticos que formam o diretório dist
+app.use (express.static (__dirname + '/ dist'));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/');
+app.get ('/ *', function (req, res) {
 
-app.get('*'), (res,req) =>{
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-
-}*/
-
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-
-var api = require('./routes/api');
-var app = express();
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/', express.static(path.join(__dirname, 'dist')));
-app.use('/api', api);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+res.sendFile (path.join (__dirname + '/ dist / index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// Inicie o aplicativo ouvindo na porta padrão do Heroku
+app.listen (process.env.PORT || 8080);
