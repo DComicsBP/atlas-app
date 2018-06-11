c20c4d2f9bd53910e0204d39ac7256d40438d802
DComicsBP <dayonne.p@gmail.com>
2018-06-10 21:03:49 -0300

    l

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require ('express');
-const path = require ('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-const app = express ();
+const app = express();
 
-// Serve apenas os arquivos estáticos que formam o diretório dist
-app.use (express.static (__dirname + '/ dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.get ('/ *', function (req, res) {
+app.get('/*', function(req,res) {
 
-res.sendFile (path.join (__dirname + '/ dist / index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-// Inicie o aplicativo ouvindo na porta padrão do Heroku
-app.listen (process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
